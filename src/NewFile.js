88b3faa0b3e4edfa61ffafcf51cdaa027cfd3c90
Ayantika <ayantikamukh6767@gmail.com>
2023-01-11 17:55:16 +0530

    First Commit

---
+++ src/NewFile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Banner from "./Banner";
+import "./Banner.css";
+import "./NewFile.css";
+
+
+function NewFile(){
+    return <div>
+        <p>jhdfchcbhvhf</p>
+    </div>
+}
+
+export default NewFile;
